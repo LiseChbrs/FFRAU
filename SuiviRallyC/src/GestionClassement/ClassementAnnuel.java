62cdf7dc1ac1e125e644f12750581e47c637f25f
Zallenx <sunyi19950601@gmail.com>
2019-10-21 15:00:29 +0200

    ClassementAnnuel

--- SuiviRallyC/src/GestionClassement/ClassementAnnuel.java
+++ SuiviRallyC/src/GestionClassement/ClassementAnnuel.java
@@ -1,5 +1,19 @@
 package GestionClassement;
 
+import java.util.ArrayList;
+
+import GestionRallye.EditionRallye;
+
 public class ClassementAnnuel {
+	ArrayList<EditionRallye> listER;
+	public ClassementAnnuel() {
+		this.listER = new ArrayList<EditionRallye>();
+	}
+	
+	public void addEditionRallye(EditionRallye er) {
+		this.listER.add(er);
+	}
+	
+	
 
 }
